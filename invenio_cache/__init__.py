73911ce57b18f64b7a9a22cf26e5edb93745418c
Maximilian Moser <maximilian.moser@tuwien.ac.at>
2026-07-16 16:32:18 +0200

    release: v3.0.1

--- invenio_cache/__init__.py
+++ invenio_cache/__init__.py
@@ -1,5 +1,6 @@
 # SPDX-FileCopyrightText: 2017-2018 CERN.
 # SPDX-FileCopyrightText: 2024-2026 Graz University of Technology.
+# SPDX-FileCopyrightText: 2026 TU Wien.
 # SPDX-License-Identifier: MIT
 
 """Cache module for Invenio.
@@ -73,7 +74,7 @@ from .decorators import cached_unless_authenticated
 from .ext import InvenioCache
 from .proxies import current_cache, current_cache_ext
 
-__version__ = "3.0.0"
+__version__ = "3.0.1"
 
 __all__ = (
     "__version__",
